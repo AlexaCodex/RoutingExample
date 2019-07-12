0f6a5db4f536d8dfb413b6b4d5f31250f9db26bc
AlexaCodex <4513589+AlexaCodex@users.noreply.github.com>
2019-07-12 16:49:12 +0100

    initial commit

---
+++ RoutingExample/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoutingExample.Controllers
+{   
+    [ApiController]
+    //Raj added this, if not there already
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "V 1";
+        }
+    }
+}
---
+++ RoutingExample/Controllers/ValuesV2Controller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoutingExample.Controllers
+{
+    
+    [ApiController]
+    //Raj added this
+    [ApiVersion("2.0")]
+    [Route("api/v{version:apiVersion}/Values")]
+    public class ValuesV2Controller : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "V 2";
+        }
+    }
+}
